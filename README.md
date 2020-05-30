f575878a8406d3e4fd6c073d91e27cd2e9aba622
Sébastien Guillon <sebastienguillon@gmail.com>
2020-05-30 23:09:04 +0200

    Create Capacitor Geolocation demo

---
+++ README.md
@@ -0,0 +1,3 @@
+# Capacitor Geolocation demo
+
+This is a simple demo of the [Geolocation API in Capacitor](https://capacitor.ionicframework.com/docs/apis/geolocation).
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,39 @@
 import { Component } from '@angular/core';
 
+import { GeolocationPosition, GeolocationOptions, Plugins } from '@capacitor/core';
+const { Geolocation } = Plugins;
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  currentPos: GeolocationPosition;
+  waitingForCurrentPosition = false;
+  geolocationOptions: GeolocationOptions = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 20000,
+  };
 
   constructor() {}
+
+  async requestPermission() {
+    const permissions = await Geolocation.requestPermissions();
+    console.log('permissions:', permissions);
+  }
+
+  async getCurrentPos() {
+    try {
+      this.waitingForCurrentPosition = true;
+      this.currentPos = await Geolocation.getCurrentPosition(this.geolocationOptions);
+      console.log(this.currentPos);
+    } catch (err) {
+      console.error('Failed to get current position.', err);
+    } finally {
+      this.waitingForCurrentPosition = false;
+    }
+  }
 
 }
