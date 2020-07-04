01284f24f23b3481eef5921a618b80dab88b50a7
Sébastien Guillon <sebastienguillon@gmail.com>
2020-07-04 16:46:48 +0200

    Add watch position demo

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
 
 import { GeolocationPosition, GeolocationOptions, Plugins } from '@capacitor/core';
 const { Geolocation } = Plugins;
@@ -9,15 +9,28 @@ const { Geolocation } = Plugins;
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  // For current position
   currentPos: GeolocationPosition;
   waitingForCurrentPosition = false;
+
+  // For position watch
+  watchedPos: GeolocationPosition;
+  watchingPosition = false;
+  locationWatchId: string;
+
+  // Toggle options display
+  showOptions = false;
+
+  // Geolocation options
   geolocationOptions: GeolocationOptions = {
     enableHighAccuracy: true,
     timeout: 5000,
     maximumAge: 20000,
   };
 
-  constructor() {}
+  constructor(
+    private ngZone: NgZone,
+  ) { }
 
   async requestPermission() {
     const permissions = await Geolocation.requestPermissions();
@@ -36,4 +49,32 @@ export class HomePage {
     }
   }
 
+  watchPos() {
+    this.watchingPosition = true;
+    this.locationWatchId = Geolocation.watchPosition(this.geolocationOptions, (position, err) => {
+      this.ngZone.run(() => {
+        if (err) {
+          console.error('Failed to watch position.', err);
+        }
+
+        this.watchedPos = position;
+        console.log(position);
+      });
+    });
+  }
+
+  async clearWatchPos() {
+    try {
+      if (this.locationWatchId) {
+        await Geolocation.clearWatch({
+          id: this.locationWatchId,
+        });
+        console.log('Cleared watch');
+      }
+    } catch (err) {
+      console.error('Failed to clear watch.', err);
+    } finally {
+      this.watchingPosition = false;
+    }
+  }
 }
